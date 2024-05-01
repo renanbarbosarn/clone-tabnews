0c729fed8b02feb0fc305ecd0a79b1b0377ffb05
Renan <renanisdeveloper@gmail.com>
2024-05-01 18:28:50 +0000

    adiciona os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 function Home() {
-    return <div><h1>Aqui é o começo</h1> <p>O início de um projeto muito top, te garanto. Claro, como esse é um repositório público, fique a vontante para dar uma olhada em seu desenvolvimento. E, até as próximas atalizações desse projeto, que será muito massa!!👋🏻😁</p></div>
+  return (
+    <div>
+      <h1>Aqui é o começo</h1>
+      <p>
+        O início de um projeto muito top, te garanto. Claro, como esse é um
+        repositório público, fique a vontante para dar uma olhada em seu
+        desenvolvimento. E, até as próximas atalizações desse projeto, que será
+        muito massa!!👋🏻😁
+      </p>
+    </div>
+  );
 }
 
 export default Home;
